f6f5d97306ae491b8fa29bfa49a08ab94c6e2a21
Oleg <ghimpco@gmail.com>
2024-07-04 01:16:45 +0300

    Respond with an echo

---
+++ src/main/java/dev/oxyac/skinulibot/config/TelegramClient.java
@@ -0,0 +1,13 @@
+package dev.oxyac.skinulibot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+
+@Component
+public class TelegramClient extends OkHttpTelegramClient {
+    public String token;
+    public TelegramClient(@Value("${bot.token}") String token) {
+        super("bow%s".formatted(token));
+    }
+}
--- src/main/java/dev/oxyac/skinulibot/config/TelegramConfig.java
+++ src/main/java/dev/oxyac/skinulibot/config/TelegramConfig.java
@@ -3,8 +3,11 @@ package dev.oxyac.skinulibot.config;
 import dev.oxyac.skinulibot.service.WebhookService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.api.methods.GetMe;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 import org.telegram.telegrambots.webhook.starter.SpringTelegramWebhookBot;
 import org.telegram.telegrambots.webhook.starter.TelegramBotsSpringWebhookApplication;
 @Configuration
@@ -12,9 +15,12 @@ public class TelegramConfig {
     public final TelegramBotsSpringWebhookApplication telegramApplication;
     private final WebhookService webhookService;
 
-    public TelegramConfig(TelegramBotsSpringWebhookApplication telegramApplication, WebhookService webhookService) {
+    private final TelegramClient telegramClient;
+
+    public TelegramConfig(TelegramBotsSpringWebhookApplication telegramApplication, WebhookService webhookService, TelegramClient telegramClient) {
         this.telegramApplication = telegramApplication;
         this.webhookService = webhookService;
+        this.telegramClient = telegramClient;
 
         webhookBot();
     }
@@ -23,7 +29,21 @@ public class TelegramConfig {
             telegramApplication.registerBot(
                     "update",
                     update -> {
-                        return new GetMe();
+                        String message_text = update.getMessage().getText();
+                        long chat_id = update.getMessage().getChatId();
+
+                        SendMessage message = SendMessage // Create a message object
+                                .builder()
+                                .chatId(chat_id)
+                                .text(message_text)
+                                .build();
+                        try {
+                            telegramClient.execute(message); // Sending our message object to user
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+
+                        return null;
                     },
                     webhookService::assignToken,
                     webhookService::removeToken
