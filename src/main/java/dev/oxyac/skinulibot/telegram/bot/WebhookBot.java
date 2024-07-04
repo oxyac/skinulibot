a49ec6811d518a54df0639b79af6d100c62e363d
Oleg <ghimpco@gmail.com>
2024-07-04 15:02:52 +0300

    Inline query

--- src/main/java/dev/oxyac/skinulibot/telegram/bot/WebhookBot.java
+++ src/main/java/dev/oxyac/skinulibot/telegram/bot/WebhookBot.java
@@ -4,9 +4,11 @@ import dev.oxyac.skinulibot.service.WebhookService;
 import dev.oxyac.skinulibot.telegram.TelegramClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery;
 import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultsButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
@@ -21,39 +23,58 @@ public class WebhookBot extends SpringTelegramWebhookBot {
 
     private final WebhookService webhookService;
     private final TelegramClient telegramClient;
+
     public WebhookBot(WebhookService webhookService, TelegramClient telegramClient) {
         super(
                 "update",
                 update -> {
                     log.debug("Received update: {}", update);
 
-                    String message_text = update.getMessage().getText();
-                    long chat_id = update.getMessage().getChatId();
+
+                    if (update.hasInlineQuery()) {
+                        AnswerInlineQuery answerInlineQuery = AnswerInlineQuery.builder()
+                                .inlineQueryId(update.getInlineQuery().getId())
+                                .button(InlineQueryResultsButton
+                                        .builder()
+                                        .text("All members")
+                                        .startParameter("")
+                                        .build())
+                                .build();
+                        try {
+                            telegramClient.execute(answerInlineQuery); // Sending our message object to user
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (update.hasMessage()) {
+                        String message_text = update.getMessage().getText();
+                        long chat_id = update.getMessage().getChatId();
 
 
-                    SendMessage message = SendMessage // Create a message object
-                            .builder()
-                            .chatId(chat_id)
-                            .text(message_text)
-                            .replyMarkup(InlineKeyboardMarkup
-                                    .builder()
-                                    .keyboardRow(
-                                            new InlineKeyboardRow(InlineKeyboardButton
-                                                    .builder()
-                                                    .text("Update message text")
-                                                    .callbackData("update_msg_text")
-                                                    .build()
-                                            )
-                                    )
-                                    .build())
-                            .build();
-                    log.debug("Message: {}", message.toString());
-
-                    try {
-                        telegramClient.execute(message); // Sending our message object to user
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
+                        SendMessage message = SendMessage // Create a message object
+                                .builder()
+                                .chatId(chat_id)
+                                .text(message_text)
+                                .replyMarkup(InlineKeyboardMarkup
+                                        .builder()
+                                        .keyboardRow(
+                                                new InlineKeyboardRow(InlineKeyboardButton
+                                                        .builder()
+                                                        .text("Update message text")
+                                                        .callbackData("update_msg_text")
+                                                        .build()
+                                                )
+                                        )
+                                        .build())
+                                .build();
+                        try {
+                            telegramClient.execute(message); // Sending our message object to user
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
                     }
+
+
+
 
                     return null;
                 },
